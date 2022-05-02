dc8119e8d6b6ccf5fbed4bd95cdf5b6a95de3c8d
kamatrajesh <104766746+kamatrajesh@users.noreply.github.com>
2022-05-02 19:31:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,3 +26,11 @@ resource "aws_instance" "ubuntu" {
     Name = var.instance_name
   }
 }
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
